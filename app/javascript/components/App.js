f3dce7537205d0bd48436dc51cde095db4fcb963
white3d <odhiambo.edward111@gmail.com>
2022-10-18 18:26:08 +0300

    Add ConfigureStore

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import store from './store/index';
+import store from './store/configureStore';
 import Greeting from './Greeting';
 
 const App = () => {
