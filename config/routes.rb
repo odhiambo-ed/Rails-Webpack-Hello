ce1cec68632985c23260590b01d3285c7c450c53
white3d <odhiambo.edward111@gmail.com>
2022-10-31 10:16:06 +0300

    Add API Routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
-  get 'root/index'
-  get 'home/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   root "root#index"
+
+  namespace :api do 
+    namespace :v1 do
+      get "greetings" => "greetings#index"
+    end
+  end
 end
