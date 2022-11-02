fd936185f0e43f21dfb28c2597078a89b298d4b4
white3d <odhiambo.edward111@gmail.com>
2022-11-02 09:09:20 +0300

    Edit API Controller

--- app/controllers/api/v1/greetings_controller.rb
+++ app/controllers/api/v1/greetings_controller.rb
@@ -2,9 +2,9 @@ class Api::V1::GreetingsController < ApplicationController
   def index
     @greetings = Greeting.all
     if @greetings.empty?
-      json_response({ msg: 'No Greetings Available at the moment' }, 400)
+      json_response({ msg: 'No Greetings at this time' }, 400)
     else
-      render json: @greeting
+      render json: @greetings[rand(0...@greetings.length - 1)]
     end
   end
 end
