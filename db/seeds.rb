1ec55a2e1d8248f8ab4d85a21357fadaf9b8fa8e
white3d <odhiambo.edward111@gmail.com>
2022-11-02 09:11:25 +0300

    Added Seed Data

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,8 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-Greeting.create(greeting: "Hi there!! Hope you are doing well")
-Greeting.create(greeting: "Hi there!! Hope you are doing well")
-Greeting.create(greeting: "Hi there!! Hope you are doing well")
-Greeting.create(greeting: "Hi there!! Hope you are doing well")
-Greeting.create(greeting: "Hi there!! Hope you are doing well")
+Greeting.create(message: "Hi there!! Hope you are doing well")
+Greeting.create(message: "Hi there!! Hope you are doing well")
+Greeting.create(message: "Hi there!! Hope you are doing well")
+Greeting.create(message: "Hi there!! Hope you are doing well")
+Greeting.create(message: "Hi there!! Hope you are doing well")
